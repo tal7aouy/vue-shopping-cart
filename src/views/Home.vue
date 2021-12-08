402b3af578a3a3cd3a60ff5adbf7afae65f57e99
tal7aouy <talhaouycoding@gmail.com>
2021-12-08 12:19:14 +0100

    add products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="home"></div>
+  <app-layout>
+    <template #header>
+      <Navbar />
+    </template>
+    <products-grid />
+  </app-layout>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import AppLayout from '@/components/layout/AppLayout.vue'
+import Navbar from '@/components/Navbar.vue'
+import ProductsGrid from '@/components/ProductsGrid.vue'
 export default {
   name: 'Home',
-  components: {},
+  components: { AppLayout, Navbar, ProductsGrid },
 }
 </script>
