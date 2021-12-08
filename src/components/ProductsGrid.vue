72ca4f7895baf97f9daf83f2da16ef6c72599b38
tal7aouy <talhaouycoding@gmail.com>
2021-12-08 12:24:08 +0100

    ADD:Products

--- src/components/ProductsGrid.vue
+++ src/components/ProductsGrid.vue
@@ -4,18 +4,19 @@
     <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-4">
       <!-- product item -->
       <div
-        v-for="(item, index) of items"
-        :key="index"
+        v-for="product in products"
+        :key="product.id"
         class="flex flex-col text-justify bg-gray-300 rounded-md shadow-lg"
       >
-        <img src="/img/1.jpg" class="p-2 rounded-md" alt="" />
-        <h4 class="text-center font-medium text-gray-700">T-shirt InHale</h4>
+        <img :src="product.photo" class="p-2 rounded-md" alt="" />
+        <h4 class="text-center font-medium text-gray-700">
+          {{ product.name }}
+        </h4>
         <div class="flex items-center justify-around my-3">
           <span class="font-semibold text-gray-900">{{
-            formatNumber(20)
+            formatNumber(product.price)
           }}</span>
           <button
-            @click="changeState"
             type="button"
             class="bg-indigo-500 px-4 py-1 text-white rounded-md transition-all duration-200 ease-in-out hover:bg-indigo-700 font-serif transform hover:scale-105"
           >
@@ -32,11 +33,12 @@
 <script>
 import { ref } from 'vue'
 import { formatNumber } from '@/utils/helpers'
+import { data } from '@/utils/data'
 export default {
   name: 'ProductsGrid',
   setup() {
-    const items = ref(10)
-    return { items, formatNumber }
+    const products = ref(data)
+    return { products, formatNumber }
   },
 }
 </script>
