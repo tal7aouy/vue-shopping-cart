53a0a697ac977dcd499460f876b261a4e242896b
tal7aouy <talhaouycoding@gmail.com>
2021-12-08 12:09:48 +0100

    ADD:layout

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full">
+    <slot name="header"></slot>
+  </div>
+  <main class="w-full">
+    <slot></slot>
+  </main>
+</template>
+<script>
+export default {
+  name: 'AppLayout',
+}
+</script>
